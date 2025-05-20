183ce8dbc30cc77fe1692aded59a15a6eb34a28f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-05-20 20:40:56 -0300

    ✨ feat: cria componente Text

---
+++ src/components/Text/index.tsx
@@ -0,0 +1,24 @@
+import { ElementType } from 'react'
+import { Box } from '@/components/Box'
+import { PolymorphicProps } from '@/types/Polymorphic'
+import styles from './Text.module.css'
+
+interface TextProps {
+    className?: string
+}
+
+export function Text<T extends ElementType = 'span'>({
+    as,
+    className = '',
+    ...props
+}: PolymorphicProps<T, TextProps>) {
+    const Tag = as || 'span'
+
+    return (
+        <Box
+            as={Tag}
+            className={`${styles.text} ${className}`}
+            {...props}
+        />
+    )
+}
