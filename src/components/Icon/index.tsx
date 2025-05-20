0c3dfa5b806c1a159bdcf7a5a4890d4c27a864cd
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-05-20 20:38:15 -0300

    ✨ feat: cria componente Icon

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,28 @@
+import * as iconSet from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Box } from '@/components/Box'
+import styles from './Icon.module.css'
+
+function capitalize(str: string) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+interface IconProps {
+    iconVariant: string
+    className?: string
+}
+
+export function Icon({ iconVariant, className = '', ...props }: IconProps) {
+    const iconName = `fa${capitalize(iconVariant)}` as keyof typeof iconSet
+    const icon = iconSet[iconName] as iconSet.IconDefinition
+
+    return (
+        <Box
+            as={FontAwesomeIcon}
+            icon={icon}
+            crossOrigin="anonymous"
+            className={`${styles.icon} ${className}`}
+            {...props}
+        />
+    )
+}
