62ceabd2b9b3d1d74da2907af47d67f866edd5ce
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-05-20 15:55:24 -0300

    ♻️ refactor: altera CSS-in-JS por CSS modules

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { JSX } from 'react'
+import styles from './Title.module.css'
 
 interface TitleProps {
     children: React.ReactNode
@@ -11,14 +12,6 @@ export default function Title({ children, as }: TitleProps) {
     const Tag = as as React.ElementType
 
     return (
-        <>
-            <Tag>{children}</Tag>
-            <style jsx>{`
-                ${Tag} {
-                    color: red;
-                    font-family: sans-serif;
-                }
-            `}</style>
-        </>
+        <Tag className={styles.titulo}>{children}</Tag>
     )
 }
