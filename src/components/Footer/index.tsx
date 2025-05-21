827cf6b8434f53599620ee2abdae323ce8214fba
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-05-21 18:22:58 -0300

    ✨ feat: cria componente de rodapé

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+import { Box } from '@/components/Box'
+import { Text } from '@/components/Text'
+import styles from './Footer.module.css'
+
+export default function Footer() {
+    return (
+        <Box className={styles.rodape} as="footer">
+            <Box className={styles.conteudo}>
+                <Text className={styles.texto} as="p">
+                    © {new Date().getFullYear()} Gabriel Veroneze. Todos os direitos
+                    reservados.
+                </Text>
+            </Box>
+        </Box>
+    )
+}
