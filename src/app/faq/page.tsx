3fa1393d828d947afb678a4abd091d4685fdbef5
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-05-24 20:08:56 -0300

    ✨ feat: busca dados da API e renderizar perguntas e respostas dinamicamente

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,10 +1,32 @@
+import { FAQ } from '@/types/FAQ'
 import Link from 'next/link'
 
-export default function FAQPage() {
+async function getFAQ(): Promise<FAQ[]> {
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+
+    const response = await fetch(FAQ_API_URL)
+    const FaqData: FAQ[] = await response.json()
+
+    return FaqData
+}
+
+export default async function FAQPage() {
+    const faq = await getFAQ()
+
     return (
         <div>
             <h1>Alura Cases - Página de Perguntas FAQ</h1>
             <Link href="/">Ir para a home</Link>
+            <ul>
+                {faq.map(({ question, answer }, index) => (
+                    <li key={index}>
+                        <article>
+                            <h2>{question}</h2>
+                            <p>{answer}</p>
+                        </article>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
